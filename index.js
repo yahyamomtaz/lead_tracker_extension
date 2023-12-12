2e099685272f138b3542f6d9e95e4dc187eeba96
yahyamomtaz <yahyamomtaz@gmail.com>
2023-12-12 19:04:10 +0100

    delete button deployed

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 let myLeads = []
 
 let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsLocalStorage)
+
+if (leadsLocalStorage)  {
+    myLeads = leadsLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
@@ -19,7 +24,6 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
