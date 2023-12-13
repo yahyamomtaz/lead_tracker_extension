bae33c60580f99e016285e6663103a40fc99bad5
yahyamomtaz <yahyamomtaz@gmail.com>
2023-12-13 12:47:59 +0100

    ready to publish

--- index.js
+++ index.js
@@ -2,38 +2,51 @@ const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const tabBtn = document.getElementById("tab-btn");
 let myLeads = []
 
 let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsLocalStorage)  {
     myLeads = leadsLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-
-    renderLeads()
-
-    console.log(localStorage.getItem("myLeads"))
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
 }
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs,query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+    render(myLeads)
+})
+
 
 /*
 inputBtn.addEventListener("click", function() {
